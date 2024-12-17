ff01b96fad7c5f403f5690b978c4a4c72d3415b0
Emerson Gomes <181042333+user-EGS@users.noreply.github.com>
2024-12-16 22:54:13 -0300

    ex00 / teste

---
+++ teste/comentarios.py
@@ -0,0 +1,13 @@
+#Isto eh um comentario
+print("Ola Mundo")
+
+#outro comentario
+print("nova linha")
+
+"""
+comentarios 
+de 
+muitas 
+linhas
+"""
+#-*- coding: utf-8 -*- server para importar os acentos das palavras
